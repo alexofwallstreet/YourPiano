de0e532e03bcd657d58cc3fc87f4132ff9235ce8
alexofwallstreet <alexofwallstreet@gmail.com>
2022-03-26 11:03:37 +0300

    project init

---
+++ vue/src/store/index.js
@@ -0,0 +1,54 @@
+import {createStore} from "vuex";
+import axiosClient from '../axios'
+
+const store = createStore({
+  state: {
+    user: {
+      data: {},
+      token: sessionStorage.getItem('TOKEN')
+    }
+  },
+  getters: {},
+  actions: {
+    register({ commit }, user) {
+      return axiosClient.post('/register', user)
+        .then(({data}) => {
+          commit('setUser', data);
+          return data;
+        })
+    },
+
+    login({ commit }, user) {
+      return axiosClient.post('/login', user)
+        .then(({data}) => {
+          commit('setUser', data);
+          return data;
+        })
+    },
+
+    logout({ commit }) {
+      return axiosClient.post('/logout')
+        .then(res => {
+          commit('logout');
+          return res;
+        })
+    }
+
+  },
+
+  mutations: {
+    logout: state => {
+      state.user.data = {};
+      state.user.token = null;
+      sessionStorage.clear();
+    },
+    setUser: (state, userData) => {
+      state.user.token = userData.token;
+      state.user.data = userData.user;
+      sessionStorage.setItem('TOKEN', userData.token);
+    }
+  },
+  modules: {}
+})
+
+export default store;
---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <PageComponent title="Dashboard">
+    Content Some
+  </PageComponent>
+</template>
+
+<script>
+import PageComponent from "../components/PageComponent.vue";
+export default {
+  name: "Dashboard",
+  components: {PageComponent}
+}
+</script>
+
+<style scoped>
+
+</style>
